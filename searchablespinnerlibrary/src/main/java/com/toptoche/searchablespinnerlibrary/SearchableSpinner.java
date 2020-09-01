fe5915bf6d2ec3779da3daff6a35840d6aea0d9c
Ahmad Romadhon <romadhon1byar@gmail.com>
2020-09-01 10:06:05 +0700

    Update SearchableSpinner.java

--- searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableSpinner.java
+++ searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableSpinner.java
@@ -16,8 +16,8 @@ import android.widget.SpinnerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.support.annotation.NonNull;;
+import android.support.annotation.Nullable;
 
 import org.jetbrains.annotations.NotNull;
 
